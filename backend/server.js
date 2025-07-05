656c75c18248748f1572b3ceb71df120576a13ab
Your Name <you@example.com>
2025-07-05 23:43:10 +0530

    Dynamic CORS support for all coding platforms

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,29 @@ require('dotenv').config();
 const hintsRoute = require('./router/hintsRouter');
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'https://leetcode.com',
+  'https://www.leetcode.com',
+  'https://www.geeksforgeeks.org',
+  'https://practice.geeksforgeeks.org',
+  'https://www.hackerrank.com',
+  'https://codeforces.com',
+  'https://www.codeforces.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use('/api/hints', hintsRoute);
 
