00adbaa17ea1ba7384e540fc17d62c7aba2de7e3
Your Name <you@example.com>
2025-07-05 23:20:31 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
-const hintsRoute = require('./router/hintsRouter')
-const cors=require('cors');
-
+const hintsRoute = require('./router/hintsRouter');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/hints', hintsRoute);
 
-
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    );
-  })
-  .catch((err) => console.error('DB connection failed:', err));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
